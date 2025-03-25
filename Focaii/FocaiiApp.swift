7b36fdfe10b4a89e3d383998251d3ce8c07dcc0f
Siluni <silunienara@gmail.com>
2025-03-25 12:42:08 +0530

    First commit

---
+++ Focaii/FocaiiApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocaiiApp.swift
+//  Focaii
+//
+//  Created by Siluni 025 on 2025-03-25.
+//
+
+import SwiftUI
+
+@main
+struct FocaiiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FocaiiTests/FocaiiTests.swift
@@ -0,0 +1,17 @@
+//
+//  FocaiiTests.swift
+//  FocaiiTests
+//
+//  Created by Siluni 025 on 2025-03-25.
+//
+
+import Testing
+@testable import Focaii
+
+struct FocaiiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
